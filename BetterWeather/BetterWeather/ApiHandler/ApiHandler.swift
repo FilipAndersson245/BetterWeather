d826af565bdad97ea8194e0436b6fe4dec720dcf
Filip Andersson <anfi1622@mace2428-11.jth-stud2.hj.se>
2018-11-06 11:41:09 +0100

    Added template functions to the ApiHandler class

---
+++ BetterWeather/BetterWeather/ApiHandler/ApiHandler.swift
@@ -0,0 +1,41 @@
+//
+//  fetcher.swift
+//  BetterWeather
+//
+//  Created by Filip Andersson on 2018-11-06.
+//  Copyright © 2018 Jonatan Flyckt, Filip Andersson, Simon Arvidson, Marcus Gullstrand. All rights reserved.
+//
+
+import Foundation
+
+
+
+class ApiHandler {
+    private func fetch() {
+    
+    }
+    
+    private static func jsonToDB() {
+        
+    }
+    
+    private static func dbToObject() {
+        
+    }
+    
+    private static func overviewObject() {
+        
+    }
+    
+    private static func dayObject() {
+    
+    }
+    
+    private static func hourObject() {
+        
+    }
+    
+    public static func foo<T>(_ lon: Float, _ lat: Float) {
+        
+    }
+}
