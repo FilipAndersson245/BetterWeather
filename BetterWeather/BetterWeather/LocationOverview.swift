c9eb087b545203ed1f317e3da818e273a4e2d5ed
Simon Arvidsson <arsi1632@mace2428-12.jth-stud1.hj.se>
2018-11-06 09:04:15 +0100

    Added models and controller for Location Overview cell.

---
+++ BetterWeather/BetterWeather/LocationOverview.swift
@@ -0,0 +1,11 @@
+//
+//  LocationOverview.swift
+//  BetterWeather
+//
+//  Created by Simon Arvidsson on 2018-11-06.
+//  Copyright © 2018 Jonatan Flyckt, Filip Andersson, Simon Arvidson, Marcus Gullstrand. All rights reserved.
+//
+
+class LocationOverview {
+
+}
---
+++ BetterWeather/BetterWeather/LocationOverviewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationOverviewTableViewCell.swift
+//  BetterWeather
+//
+//  Created by Simon Arvidsson on 2018-11-06.
+//  Copyright © 2018 Jonatan Flyckt, Filip Andersson, Simon Arvidson, Marcus Gullstrand. All rights reserved.
+//
+
+import UIKit
+
+class LocationOverviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
