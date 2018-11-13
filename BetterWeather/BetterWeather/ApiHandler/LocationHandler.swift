301778d0443ffc66152f44c23d1edab8b6ef225c
Marcus Gullstrand <guma1502@mace2428-16.jth-stud2.hj.se>
2018-11-13 11:31:49 +0100

    Started adding map functionality.

---
+++ BetterWeather/BetterWeather/ApiHandler/LocationHandler.swift
@@ -0,0 +1,10 @@
+//
+//  LocationHandler.swift
+//  BetterWeather
+//
+//  Created by Marcus Gullstrand on 2018-11-13.
+//  Copyright © 2018 Jonatan Flyckt, Filip Andersson, Simon Arvidson, Marcus Gullstrand. All rights reserved.
+//
+
+import Foundation
+
