7408a0d9c960529b9d7ba82787680e7046fe336a
Jonatan Flyckt <fljo1589@mace2428-11.jth-stud2.hj.se>
2018-11-28 11:42:25 +0100

    Removes shitt code.

--- BetterWeather/BetterWeather/OverviewViewController.swift
+++ BetterWeather/BetterWeather/OverviewViewController.swift
@@ -68,12 +68,7 @@ class OverviewViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let dbHandler = DatabaseHandler() //temporary
-        dbHandler.createDataTable() //temporary
-        //dbHandler.insertData() //temporary
-        //dbHandler.readData() //temporary
-        
+       
         loadSampleLocations()
         loadCurrentLocation()
         
